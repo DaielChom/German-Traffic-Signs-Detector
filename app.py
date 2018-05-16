5f15803fb0676513e58649aadec511bf9d84ef4d
DaielChom <danielcarvajalpatino@gmail.com>
2018-05-16 17:02:35 -0500

    :gem: define download Command click

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-def cli():
-    print('Hello World!')
+import click
+import utils as ut
+
+@click.group()
+@click.option('--hellow', is_flag=True)
+def cli(hellow):
+    if hellow:
+        click.echo('Hellow World')
+
+@cli.command()
+#@click.option('--download', default = False, help="Downloda Dataset")
+def download():
+    """Command for Download the Dataset"""
+    ut.download_dataset()
